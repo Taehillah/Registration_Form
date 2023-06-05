a205880caaff74cca26902c0ef75238b97d05a9e
Taehillah <ishmaelmafole@gmail.com>
2023-06-05 22:48:33 +0200

    Ammendment

--- db.php
+++ db.php
@@ -3,7 +3,7 @@ $servername='localhost';
 $username='root';
 $password='';
 $dbname='logindb';
-$conn=mysqli_connect($servername,$username,$password,"$logindb");
+$conn=mysqli_connect($servername,$username,$password,"$dbname");
 if(!$conn){
     die('Couldnt Connect to the Sql Server' .mysql_error());
 }
