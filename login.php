e1575920ef2dfd308f133531318475704ea67d87
Taehillah <ishmaelmafole@gmail.com>
2023-06-09 19:16:28 +0200

    Ammendment

--- login.php
+++ login.php
@@ -1,8 +1,41 @@
 <?php
 require_once "db.php";
 
-// Check if the form is submitted
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+// Sign Up
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['signup'])) {
+    // Retrieve form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $mobile = $_POST['mobile'];
+    $password = $_POST['password'];
+
+    // Validate form data (you can add more validation if required)
+    if (empty($name) || empty($email) || empty($mobile) || empty($password)) {
+        $error = 'Please fill in all the fields.';
+    } else {
+        // Check if the email is already registered
+        $query = "SELECT * FROM users WHERE email = '$email'";
+        $result = mysqli_query($conn, $query);
+
+        if ($result && mysqli_num_rows($result) > 0) {
+            $error = 'Email already exists. Please choose a different email.';
+        } else {
+            // Hash the password
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+            // Insert the user into the database
+            $query = "INSERT INTO users (name, email, mobile, password) VALUES ('$name', '$email', '$mobile', '$hashedPassword')";
+            if (mysqli_query($conn, $query)) {
+                $success = 'Registration successful! Please log in.';
+            } else {
+                $error = 'Error: ' . mysqli_error($conn);
+            }
+        }
+    }
+}
+
+// Sign In
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['signin'])) {
     // Retrieve form data
     $email = $_POST['email'];
     $password = $_POST['password'];
@@ -34,27 +67,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login</title>
+    <title>Signup & Login</title>
 </head>
 <body>
-    <h1>Login</h1>
+    <h1>Signup</h1>
 
-    <?php if (isset($error)) { ?>
+    <?php if (isset($error) && isset($_POST['signup'])) { ?>
         <p style="color: red;"><?php echo $error; ?></p>
     <?php } ?>
 
-    <?php if (isset($success)) { ?>
+    <?php if (isset($success) && isset($_POST['signup'])) { ?>
         <p style="color: green;"><?php echo $success; ?></p>
     <?php } ?>
 
     <form method="POST" action="">
-        <label for="email">Email:</label><br>
-        <input type="email" id="email" name="email"><br><br>
-
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password"><br><br>
-
-        <input type="submit" value="Login">
-    </form>
-</body>
-</html>
+        <label for="name">Name:</label><br>
+        <input type="text
